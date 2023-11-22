a268ba0a11808cea2837591dbd6d6086fef25ee7
Additonal Metal <67547350+AdditionalMetal@users.noreply.github.com>
2023-11-22 18:48:35 -0500

    Working version

--- hamming.c
+++ hamming.c
@@ -1,1 +1,25 @@
 #include "hamming.h"
+#include <stddef.h>
+
+int compute(const char *lhs, const char *rhs){
+  int hamming = 0;
+
+  if (lhs == NULL || rhs == NULL) // valid strings?
+    return -1;
+
+  while ( *lhs != 0 || *rhs != 0){
+    if ( (*lhs == 0 && *rhs != 0) ||
+	 (*lhs != 0 && *rhs == 0) ){ // length mismatch
+      return -1;
+    }
+
+    if ( *lhs != *rhs ){
+      hamming++;
+    }
+    
+    lhs++;
+    rhs++;
+  }
+  return hamming;
+}
+
