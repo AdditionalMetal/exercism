6d9b118127facb3de2100f90d0a6b3776cc8c857
Additonal Metal <67547350+AdditionalMetal@users.noreply.github.com>
2023-11-23 12:24:03 -0500

    Modified to be more succinct...

--- grains.c
+++ grains.c
@@ -1,16 +1,11 @@
 #include "grains.h"
 
 uint64_t square(uint8_t index){
-  if (index <= 0)
+  if ((index <= 0) || (index > 64))
     return 0;
-  if (index == 1)
-    return 1;
 
-  uint64_t rv = 2;
-  for (int i=2; i < index; i++ ){
-    rv *= 2;
-  }
-  return rv;
+  uint64_t rv = 1;
+  return(rv<<(index-1));
 }
 
 uint64_t total(void){
