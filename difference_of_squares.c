e4c3ce3c8945cbc58710a57d7363eee4b7f14a34
Additonal Metal <67547350+AdditionalMetal@users.noreply.github.com>
2023-11-23 14:28:33 -0500

    First try - inefficient

--- difference_of_squares.c
+++ difference_of_squares.c
@@ -1,1 +1,29 @@
 #include "difference_of_squares.h"
+
+unsigned int sum_of_squares(unsigned int number){
+  unsigned int sos = 0;
+
+  for (unsigned int i=0; i<= number; i++){
+    sos += i*i;
+  }
+  
+  return sos;
+}
+
+unsigned int square_of_sum(unsigned int number){
+  unsigned int sum = 0;
+
+  for (unsigned int i=0; i<= number; i++){
+    sum += i;
+  }
+
+  unsigned int square = sum * sum;
+  return (square);
+}
+
+unsigned int difference_of_squares(unsigned int number){
+  unsigned int rv = (square_of_sum(number)) -
+                    (sum_of_squares(number)); 
+  return rv;
+}
+
