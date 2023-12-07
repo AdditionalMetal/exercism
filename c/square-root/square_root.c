4c5f2efdf0bea59c9905213aeca3286b986a254b
Additonal Metal <67547350+AdditionalMetal@users.noreply.github.com>
2023-12-07 16:31:12 -0500

    Pass Test 1

--- c/square-root/square_root.c
+++ c/square-root/square_root.c
@@ -1,1 +1,13 @@
 #include "square_root.h"
+
+// No guidance on function signature...
+//  Tests are lsited as comparing uint16_t, but tests are perfect squares and end at 255...
+//  Not sure what the creaters of this exercise want...
+
+uint16_t square_root(uint16_t radicand){
+  for (uint8_t i=0; i < UCHAR_MAX; i++){
+    if ( i*i == radicand)
+      return (uint16_t)i;
+  }
+  return (uint16_t)0;
+}
